874ac98af57a01bd558982f534f4324f7cf0ea6f
kdb11 <kevin.badwi@medieinstitutet.se>
2023-04-19 00:07:20 +0200

    base layout and images

--- therestaurant/src/App.js
+++ therestaurant/src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <p>Startpage</p>
+    <div className='container'>
+      <div className='overlay'>
+      <img src='https://images.unsplash.com/photo-1481070555726-e2fe8357725c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80' alt='chickenburger'></img>
+      <img src='https://images.unsplash.com/photo-1541614101331-1a5a3a194e92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80' alt='chef'></img>
+      <img src='https://images.unsplash.com/photo-1571168136613-46401b03904e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3870&q=80' alt='restaurant'></img>
+      </div>
+    </div>
   );
 }
 
