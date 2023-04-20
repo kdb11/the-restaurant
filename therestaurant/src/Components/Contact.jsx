4690055a422da4ca357af8dac98398aab8246a42
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2023-04-20 10:38:07 +0200

    inputs contact page

--- therestaurant/src/Components/Contact.jsx
+++ therestaurant/src/Components/Contact.jsx
@@ -1,6 +1,25 @@
+import React from 'react';
+import './Contact.css';
+
 export const Contact = () => {
 
-    return (
-        <div>contact us</div>
+    return ( 
+        <div className='form-box'>
+            <form>
+                <div className='form-group'>
+                    <input type="text" placeholder="Your Name" className='form-input'></input>
+                </div>
+                <div className='form-group'>
+                    <input type="email" placeholder="Email" className='form-input'></input>
+                </div>
+                <div className='form-group'>
+                    <textarea placeholder="Your message" rows="5" className='form-input'></textarea>
+                </div>
+                <div>
+                    <button type="submit" className='btn'>Send a message</button>
+                </div>
+
+        </form>
+        </div>
     )
 };
