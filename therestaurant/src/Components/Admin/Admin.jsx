baf03bfbf2b3a407c7361e2bf39894b332cea2f0
TwoPlusTwoMakeFour <112890961+TwoPlusTwoMakeFour@users.noreply.github.com>
2023-04-27 14:03:37 +0200

    Compare resolved

--- therestaurant/src/Components/Admin/Admin.jsx
+++ therestaurant/src/Components/Admin/Admin.jsx
@@ -1,10 +1,5 @@
 import AdminLogin from './AdminLogin';
 import './Admin.css';
-<<<<<<< Updated upstream
-export const Admin = () => {
-
-  return (<>
-=======
 import React, { useState, useEffect } from "react";
 import { MappingTest, bookingCreate, bookingEdit, bookingFetch, bookingRemove, bookings, web3 } from "../contractFunctions";
 export const Admin = () => {
@@ -118,7 +113,6 @@ export const Admin = () => {
       {mappedBookings}<br/>
       {mappedList}
     </div>
->>>>>>> Stashed changes
     <div className='adminContainer'>
       <img className='adminImg' src='https://images.unsplash.com/photo-1489528792647-46ec39027556?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt='takeAway'></img>
       <AdminLogin />
